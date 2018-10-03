ca2d4431212215d5a9d55800a5200e89ce5bc3c9
Till Kamppeter <till.kamppeter@gmail.com>
2018-10-03 15:13:00 +0200

    DEBIAN: Call QMessageBox constructors of PyQT5 with the correct parameters
    The whole Python source code of HPLIP contains 40 incorrect calls of
    the QMessageBox constructurs, mostly the list of buttons was given as
    separate arguments instead of a bitwise OR of each button's
    representation value.  If the flow of the code hits one of these
    calls (usually when some error or warnings message should be
    displayed or an "Are you sure?" question asked) the GUI app crashes.
    See https://pythonspot.com/pyqt5-messagebox/ for QMessageBox
    documentation.
    This patch corrects all these calls.
    
    Bug-Debian: https://bugs.debian.org/912768
    Bug-Ubuntu: https://bugs.launchpad.net/bugs/1745383
    Signed-off-by: Mingcong Bai <jeffbai@aosc.io>

--- ui5/setupdialog.py
+++ ui5/setupdialog.py
@@ -908,7 +908,7 @@ class SetupDialog(QDialog, Ui_Dialog):
                                     self.windowTitle(),
                                     warn_text,
                                     QMessageBox.Yes |
-                                    QMessageBox.No |
+                                    QMessageBox.No,
                                     QMessageBox.NoButton) == QMessageBox.Yes):
                 i = 2
                 while True:
@@ -1193,7 +1193,7 @@ class SetupDialog(QDialog, Ui_Dialog):
                     if QMessageBox.critical(self,
                                             self.windowTitle(),
                                             error_text,
-                                            QMessageBox.Retry | QMessageBox.Default,
+                                            QMessageBox.Retry | QMessageBox.Default |\
                                             QMessageBox.Cancel | QMessageBox.Escape,
                                             QMessageBox.NoButton) == QMessageBox.Cancel:
                         break
@@ -1245,7 +1245,7 @@ class SetupDialog(QDialog, Ui_Dialog):
                                 if QMessageBox.critical(self,
                                                         self.windowTitle(),
                                                         error_text,
-                                                        QMessageBox.Retry | QMessageBox.Default,
+                                                        QMessageBox.Retry | QMessageBox.Default |\
                                                         QMessageBox.Cancel | QMessageBox.Escape,
                                                         QMessageBox.NoButton) == QMessageBox.Cancel:
                                     break
